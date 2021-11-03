c90a7ea7a3a529ac17933b2504aad67c171c0db7
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2021-11-03 22:27:32 +0100

    seems working, have a problem with getting the number of pages, for now you gotta explicitly enter the number of pages in the book

--- network.py
+++ network.py
@@ -1,5 +1,4 @@
 import json
-import pprint
 import time
 
 from selenium import webdriver
@@ -11,7 +10,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 capabilities = DesiredCapabilities.CHROME
-# capabilities["loggingPrefs"] = {"performance": "ALL"}  # chromedriver < ~75
 capabilities["goog:loggingPrefs"] = {"performance": "ALL"}  # chromedriver 75+
 
 chrome_options = Options()
@@ -19,31 +17,35 @@ chrome_options.binary_location = "/usr/bin/brave-browser"
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=capabilities)
-driver.get("https://www.flipsnack.com/7B76FA77C6F/guide-du-rapport-de-stage-pour-les-coles-d-ing-nieurs-en-in.html")
-# driver.switch_to.default_content()
+driver.get("link")
 frame = driver.find_element_by_tag_name('iframe')
 
 driver.switch_to.frame(frame)
 try:
     fullscreen_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'clickToRead')))
-    print("Page is ready!")
+    print("fullscreen_button is ready!")
 except TimeoutException:
     print("Loading took too much time!")
 fullscreen_button.click()
+counter = 0
+for i in range(20):
+    try:
+        next_slide_button = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'flip-next-page')))
+        print("next_slide_button is ready!")
+    except TimeoutException:
+        print("Loading took too much time!")
 
+    counter += 1
+    driver.execute_script("arguments[0].click();", next_slide_button[1])
+    time.sleep(2)
 
-# if next_button is None == 0:
-#     time.sleep(3)
-#     next_button = driver.find_element_by_id("clickToRead")
-# if next_button is None == 0:
-#     raise RuntimeError("couldn't find wrapper'")
-# time.sleep(1)
+    # for j in next_slide_button.find_elements_by_css_selector("*"):
+    #     print(j.tag_name)
 
+def get_number_of_pages():
+    # i still need a more accurate way
+         return counter*2 #give or take
 
-# myElem = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'wrapper')))
-# elem = driver.find_element_by_class_name("wrapper")
-#
-# next_button.click()
 def process_browser_logs_for_network_events(logs):
     """
     Return only logs which have a method that start with "Network.response", "Network.request", or "Network.webSocket"
@@ -54,10 +56,6 @@ def process_browser_logs_for_network_events(logs):
         log = json.loads(entry["message"])["message"]
         if (
                 "Network.request" in log["method"]
-                #     and "params" in log
-                #     and "headers" in log[
-                # "params"] and "path" in log["params"]["headers"]
-                # and "original" in log["params"]["headers"]["path"]
         ):
             result.append(log)
     new_dict = {}
@@ -66,18 +64,8 @@ def process_browser_logs_for_network_events(logs):
         new_dict[str(counter)] = item
         counter += 1
     with open("log_entries.json", "wt") as out:
-        json.dump(new_dict,out)
+        json.dump(new_dict, out)
 
 
-#
-logs = driver.get_log("performance")
-events = process_browser_logs_for_network_events(logs)
-# with open("log_entries.json", "wt") as out:
-#     json.dump(events, out)
-#     # counter = 0
-#     # for event in events:
-#     #     pprint.pprint(event, stream=out)
-#
-# # document.querySelector('html > body > #fsw > [id^="fsw_widget"] > #mainLayout > #clickToRead > [class="wrapper"] > [class="click-to-read"]').click()
-#
-# # document.querySelector('html > body > [class="page-wrap"] > [class="auto-height"] > #myPlayer > iframe > html')
+process_browser_logs_for_network_events(driver.get_log("performance")
+)
