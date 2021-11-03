a1e007cd639a607c2424a99483bbd5a79c663f7c
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2021-11-03 19:34:30 +0100

    so i got the logs, now working on index.py, this souldnt be commited cuz of how messed up it is, but i prefer not to lose it

--- network.py
+++ network.py
@@ -1,10 +1,14 @@
 import json
 import pprint
+import time
 
 from selenium import webdriver
-
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 capabilities = DesiredCapabilities.CHROME
 # capabilities["loggingPrefs"] = {"performance": "ALL"}  # chromedriver < ~75
@@ -16,29 +20,64 @@ chrome_options.add_argument("--start-maximized")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=capabilities)
 driver.get("https://www.flipsnack.com/7B76FA77C6F/guide-du-rapport-de-stage-pour-les-coles-d-ing-nieurs-en-in.html")
+# driver.switch_to.default_content()
+frame = driver.find_element_by_tag_name('iframe')
+
+driver.switch_to.frame(frame)
+try:
+    fullscreen_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'clickToRead')))
+    print("Page is ready!")
+except TimeoutException:
+    print("Loading took too much time!")
+fullscreen_button.click()
+
+
+# if next_button is None == 0:
+#     time.sleep(3)
+#     next_button = driver.find_element_by_id("clickToRead")
+# if next_button is None == 0:
+#     raise RuntimeError("couldn't find wrapper'")
+# time.sleep(1)
 
 
 # myElem = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'wrapper')))
 # elem = driver.find_element_by_class_name("wrapper")
 #
-# elem.click()
+# next_button.click()
 def process_browser_logs_for_network_events(logs):
     """
     Return only logs which have a method that start with "Network.response", "Network.request", or "Network.webSocket"
     since we're interested in the network events specifically.
     """
+    result = []
     for entry in logs:
         log = json.loads(entry["message"])["message"]
         if (
-                "Network.response" in log["method"] and "params" in log and "headers" in log[
-            "params"] and "content-type" in log["params"]["headers"] and "image" in
-                log["params"]["headers"]["content-type"]
+                "Network.request" in log["method"]
+                #     and "params" in log
+                #     and "headers" in log[
+                # "params"] and "path" in log["params"]["headers"]
+                # and "original" in log["params"]["headers"]["path"]
         ):
-            yield log
+            result.append(log)
+    new_dict = {}
+    counter = 0
+    for item in result:
+        new_dict[str(counter)] = item
+        counter += 1
+    with open("log_entries.json", "wt") as out:
+        json.dump(new_dict,out)
 
 
+#
 logs = driver.get_log("performance")
 events = process_browser_logs_for_network_events(logs)
-with open("log_entries.json", "wt") as out:
-    for event in events:
-        pprint.pprint(event, stream=out)
+# with open("log_entries.json", "wt") as out:
+#     json.dump(events, out)
+#     # counter = 0
+#     # for event in events:
+#     #     pprint.pprint(event, stream=out)
+#
+# # document.querySelector('html > body > #fsw > [id^="fsw_widget"] > #mainLayout > #clickToRead > [class="wrapper"] > [class="click-to-read"]').click()
+#
+# # document.querySelector('html > body > [class="page-wrap"] > [class="auto-height"] > #myPlayer > iframe > html')
